5916fc7cb53c0cd3d14cda473dd53c7dd24b29a6
Yashwanth sagar j <dhanush@qikautomation.com>
2025-03-11 16:04:00 +0530

    WIP day 110325 push

--- frontend/src/pages/landingPage/LandingPage.tsx
+++ frontend/src/pages/landingPage/LandingPage.tsx
@@ -1,11 +1,59 @@
 import ImageSlider from "../../components/ImageSlider";
 import "./LandingPage.css";
+import ProductInfoCard from "../../components/product info card/ProductInfoCard";
+import { Link } from "react-router-dom";
+
 const LandingPage = () => {
   const images = [
     "/image/1.jpg",
     "/image/2.jpg",
     "/image/3.jpg",
     "/image/4.jpg",
+  ];
+
+  const cards = [
+    {
+      title: "1Single Lane Conveyor Series 0.5 M Cooling Conveyor QIK-500CC",
+      description:
+        "PCB (Printed Circuit Board) conveyors are essential equipment within the Surface Mount Technology (SMT) assembly....",
+      readMoreLink: "test",
+    },
+    {
+      title: "2Single Lane Conveyor Series 0.5 M Inspection Conveyor QIK-500IC",
+      description:
+        "PCB (Printed Circuit Board) conveyors are essential equipment within the Surface Mount Technology (SMT)...",
+      readMoreLink: "test",
+    },
+    {
+      title: "3Single Lane Conveyor Series 0.5 M Link Conveyor QIK-500LC",
+      description:
+        "PCB (Printed Circuit Board) conveyors are essential equipment within the Surface Mount Technology (SMT) assembly...",
+      readMoreLink: "test",
+    },
+    {
+      title: "4Single Lane Conveyor Series 0.5 M Inspection Conveyor QIK-500IC",
+      description:
+        "PCB (Printed Circuit Board) conveyors are essential equipment within the Surface Mount Technology (SMT)...",
+      readMoreLink: "test",
+    },
+    {
+      title: "5Single Lane Conveyor Series 0.5 M Link Conveyor QIK-500LC",
+      description:
+        "PCB (Printed Circuit Board) conveyors are essential equipment within the Surface Mount Technology (SMT) assembly...",
+      readMoreLink: "test",
+    },
+    {
+      title: "6Single Lane Conveyor Series 0.5 M Inspection Conveyor QIK-500IC",
+      description:
+        "PCB (Printed Circuit Board) conveyors are essential equipment within the Surface Mount Technology (SMT)...",
+      readMoreLink: "test",
+    },
+    {
+      title: "7Single Lane Conveyor Series 0.5 M Link Conveyor QIK-500LC",
+      description:
+        "PCB (Printed Circuit Board) conveyors are essential equipment within the Surface Mount Technology (SMT) assembly...",
+      readMoreLink: "test",
+    },
   ];
 
   return (
@@ -38,7 +86,34 @@ const LandingPage = () => {
           </div>
         </div>
       </div>
-      <div style={{ height: "100vh" }}>test</div>
+      <div className="container-xxl py-5 cardContainer ">
+        <div className="product-card-wrapper">
+          <h6 className="section-title ">Our Product & Solutions</h6>
+          <p className="product-title">
+            Our compressive solutions aligned with Industry 4.0
+          </p>
+        </div>
+        <div className="mt-5">
+          <ProductInfoCard cards={cards} />
+        </div>
+        <Link to={"/products"} className="readme-btn-wrapper">
+          <button className="readme-btn">Read More</button>
+        </Link>
+      </div>
+      <div className="container-xxl py-5 cardContainer ">
+        <div>
+          <h6 className="section-title ">About Us</h6>
+          <p className="product-title">
+            Dedicated to revolutionizing the electronic manufacturing industry.
+          </p>
+        </div>
+        <div className="mt-5">
+          <ProductInfoCard cards={cards} withAnimation={false} />
+        </div>
+        <Link to={"/about"} className="readme-btn-wrapper">
+          <button className="readme-btn">Read More</button>
+        </Link>
+      </div>
     </>
   );
 };
